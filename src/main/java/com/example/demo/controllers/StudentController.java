2b2ba989d533abec1023269451c96c248083af8d
BrunoGurris <brunoagpires@gmail.com>
2024-11-30 15:58:40 -0300

    feat(deploy): CORS

--- src/main/java/com/example/demo/controllers/StudentController.java
+++ src/main/java/com/example/demo/controllers/StudentController.java
@@ -18,14 +18,12 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@CrossOrigin(origins = "https://ac2.ictech.dev")
 @RequestMapping("/students")
 public class StudentController {
 
